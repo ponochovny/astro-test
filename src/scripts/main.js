3383c294928f4776bd70aa9a751359f3f14274e6
ponochovny <ponochovny@gmail.com>
2025-04-26 13:05:11 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,132 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 AOS.init()
+
+function setAllVideosMuted() {
+	const videos = document.getElementsByTagName('video')
+	for (const video of videos) {
+		video.muted = true
+	}
+}
+
+// ACCORDION
+const headers = document.querySelectorAll('.accordion-item-header')
+const arrows = document.querySelectorAll('.accordion-item-arrow')
+
+function closeAccordion(body, item) {
+	body.style.maxHeight = null
+	item.classList.remove('active')
+	setAllVideosMuted()
+}
+
+headers.forEach(function (header) {
+	header.addEventListener('click', function () {
+		const item = this.parentElement
+		const body = item.querySelector('.accordion-item-body')
+
+		if (item.classList.contains('active')) {
+			closeAccordion(body, item)
+		} else {
+			document
+				.querySelectorAll('.accordion-item')
+				.forEach(function (otherItem) {
+					if (otherItem !== item) {
+						const body = otherItem.querySelector('.accordion-item-body')
+
+						closeAccordion(body, otherItem)
+					}
+				})
+
+			item.classList.add('active')
+			body.style.maxHeight = body.scrollHeight + 'px'
+		}
+	})
+})
+
+arrows.forEach(function (arrow) {
+	arrow.addEventListener('click', function (e) {
+		e.stopPropagation()
+
+		const item = this.closest('.accordion-item')
+		const body = item.querySelector('.accordion-item-body')
+		const header = item.querySelector('.accordion-item-header')
+
+		header.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start',
+		})
+
+		setTimeout(function () {
+			closeAccordion(body, item)
+		}, 10)
+	})
+})
+
+// BACK TO TOP
+const backToTopBtn = document.getElementById('backToTop')
+
+window.onscroll = function () {
+	scrollFunction()
+}
+
+function scrollFunction() {
+	const { innerHeight } = window
+	if (
+		document.body.scrollTop > innerHeight / 2 ||
+		document.documentElement.scrollTop > innerHeight / 2
+	) {
+		backToTopBtn.classList.add('show')
+	} else {
+		backToTopBtn.classList.remove('show')
+	}
+}
+
+document.getElementById('backToTop').addEventListener('click', () => {
+	// document.body.scrollTop = 0; // For Safari
+	// document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	window.scrollTo({ top: 0, behavior: 'smooth' })
+})
+
+// MODAL
+// To open modal - element with class name "modal-trigger" and onclick="openModal(event)"
+// and sibling element with class name "modal-inject" with needed modal content
+
+const modalTriggers = document.querySelectorAll('.modal-trigger')
+const modalBackdrop = document.querySelector('.backdrop')
+
+modalBackdrop.addEventListener('click', toggleModal)
+
+modalTriggers?.forEach(function (modalTrigger) {
+	modalTrigger.addEventListener('click', function (e) {
+		console.log(e)
+		const modalInjectContainer = e.target?.nextSibling.nextSibling
+		console.log(modalInjectContainer)
+		const isContent = modalInjectContainer.classList?.contains('modal-inject')
+		if (!isContent) return
+
+		const modalContent = modalInjectContainer.innerHTML
+
+		setModalContent(modalContent)
+		toggleModal()
+	})
+})
+
+// function openModal(e) {
+// 	const modalInjectContainer = e.target.nextSibling
+// 	const isContent = modalInjectContainer.classList.contains('modal-inject')
+// 	if (!isContent) return
+
+// 	const modalContent = modalInjectContainer.innerHTML
+
+// 	setModalContent(modalContent)
+// 	toggleModal()
+// }
+function setModalContent(content) {
+	const modal = document.getElementById('modal')
+	console.log(modal)
+	modal.getElementsByClassName('modal-content')[0].innerHTML = content
+}
+function toggleModal() {
+	const modal = document.getElementById('modal')
+	modal?.classList.toggle('open')
+}
